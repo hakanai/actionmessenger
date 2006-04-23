20b31917f644f0bbab7ef34471d06624974c2812
Trejkaz (pen name) <trejkaz@trypticon.org>
2006-04-23 13:35:31 +0000

    Adding everything but XMPP4R...

---
+++ test/unit/simple_messenger_test.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'simple_messenger'
+
+class SimpleMessengerTest < Test::Unit::TestCase
+  def setup
+    @mock_messenger = ActionMessenger::MockMessenger.new
+    ActionMessenger::Messenger.register('default', @mock_messenger)
+    @expected = ActionMessenger::Message.new
+  end
+  
+  def test_wakeup
+    @expected.to = 'trejkaz@trypticon.org'
+    @expected.body = 'Wake up!'
+    
+    # Test creation of the message.
+    assert_equal @expected, SimpleMessenger.create_wakeup('trejkaz@trypticon.org')
+    
+    # Test delivery.
+    SimpleMessenger.send_wakeup('trejkaz@trypticon.org')
+    assert_equal [ @expected ], @mock_messenger.deliveries
+  end
+end
---
+++ vendor/actionmessenger/lib/action_messenger/base.rb
@@ -0,0 +1,54 @@
+
+module ActionMessenger
+  class Base
+    # The message being built, to later be sent.
+    attr_accessor :message
+    
+    # The name of the messenger to use.
+    #
+    # If set to nil, or if the messenger lookup returns nil, the message won't be sent,
+    # which may be useful if you need a simple way to temporarily disable messaging.
+    #
+    # The default messenger is the messenger named 'default' in the configuration.
+    attr_accessor :messenger
+    
+    # The default messenger to use.
+    cattr_accessor :default_messenger
+    
+    class << self
+      def method_missing(method_symbol, *parameters)
+        case method_symbol.id2name
+        when /^create_([_a-z]\w*)/ then new($1, *parameters).message
+        when /^send_([_a-z]\w*)/   then new($1, *parameters).send_message
+        end
+      end
+    end
+    
+    def initialize(method_name = nil, *parameters)
+      create!(method_name, *parameters) if method_name
+    end
+    
+    def create!(method_name, *parameters)
+      initialize_defaults
+      send(method_name, *parameters)
+      
+      # TODO: Templating, ActionMailer-style.
+    end
+    
+    # Initialises default settings for this messenger.
+    def initialize_defaults
+      @messenger = @@default_messenger || 'default'
+      @message = Message.new
+    end
+    
+    # Sends the message
+    def send_message(message = @message, messenger = @messenger)
+      unless messenger.nil?
+        messenger = Messenger.find_by_name(messenger.to_s)
+        unless messenger.nil?
+          messenger.send_message(message)
+        end
+      end
+    end
+  end
+end
