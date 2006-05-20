66b7b91aafdc49cc0427ba76ac7381f79d89d062
Trejkaz (pen name) <trejkaz@trypticon.org>
2006-05-20 13:52:46 +0000

    Unit test for multiple recipients

--- test/unit/base_test.rb
+++ test/unit/base_test.rb
@@ -4,16 +4,19 @@ require 'common'
 class BaseTest < Test::Unit::TestCase
   def setup
     @messenger = ActionMessenger::MessengerRegistry.find_by_name(:mock)
+    @messenger.deliveries = []
+
     @expected = ActionMessenger::Message.new
+    @expected.subject = 'Wake up!'
+    @expected.body = 'You told me to remind you to wake up.  Well?'
+
     SimpleMessenger.received = []
   end
   
   # Tests creating and delivering a simple message.
   def test_creation_and_delivery
     @expected.to = 'trejkaz@trypticon.org/tests'
-    @expected.subject = 'Wake up!'
-    @expected.body = 'You told me to remind you to wake up.  Well?'
-    
+
     # Test creation of the message.
     assert_equal [ @expected ], SimpleMessenger.create_wakeup('trejkaz@trypticon.org/tests')
     
@@ -22,6 +25,23 @@ class BaseTest < Test::Unit::TestCase
     assert_equal [ @expected ], @messenger.deliveries
   end
 
+  # Tests creating multiple messages, to send to multiple recipients.
+  def test_multiple_recipients
+    expected1 = @expected.clone
+    expected1.to = 'user1@trypticon.org'
+    expected2 = @expected.clone
+    expected2.to = 'user2@trypticon.org'
+
+    # Creation
+    assert_equal [ expected1, expected2 ],
+                 SimpleMessenger.create_wakeup('user1@trypticon.org', 'user2@trypticon.org')
+
+    # Delivery
+    SimpleMessenger.send_wakeup('user1@trypticon.org', 'user2@trypticon.org')
+    assert_equal [ expected1, expected2],
+                 @messenger.deliveries
+  end
+  
   # Tests receiving a message.
   def test_receiving
     incoming = ActionMessenger::Message.new
@@ -42,8 +62,8 @@ class BaseTest < Test::Unit::TestCase
       attr_accessor :received
     end
 
-    def wakeup(jid)
-      recipients jid
+    def wakeup(*jids)
+      recipients [*jids]
       subject   'Wake up!'
       body      'You told me to remind you to wake up.  Well?'
     end
